ec69e26289560dedeba7b4b0bd5162534f4d7d6b
Jean Pierre Velez <vcjpierre@gmail.com>
2020-07-13 20:56:38 -0400

    Add widgets

---
+++ lib/ui/home/empty_widget.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weather/ui/ui_constants.dart';
+
+class EmptyWidget extends StatelessWidget {
+  final VoidCallback onTap;
+
+  const EmptyWidget({Key key, this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      fit: StackFit.expand,
+      children: [
+        Positioned.fill(
+          child: Image.asset(
+            'assets/welcome.jpg',
+            fit: BoxFit.cover,
+          ),
+        ),
+        SafeArea(
+          child: Center(
+            child: Container(
+              constraints: BoxConstraints(
+                maxWidth: maxPageWidth,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Image.asset(
+                    'assets/logo.png',
+                    height: 55,
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  Text(
+                    'Hi,\nWelcome',
+                    textAlign: TextAlign.start,
+                    style: TextStyle(
+                      fontSize: 34,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    'Do you like add\na new city?',
+                    textAlign: TextAlign.start,
+                  ),
+                  const SizedBox(
+                    height: 50,
+                  ),
+                  RaisedButton(
+                    color: Colors.white,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(
+                        15.0,
+                      ),
+                    ),
+                    child: Text('Add city'),
+                    onPressed: onTap,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
