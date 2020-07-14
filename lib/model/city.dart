6c99d043a30cd4cd70718ecae33e33f93a6377e6
Jean Pierre Velez <vcjpierre@gmail.com>
2020-07-13 20:56:12 -0400

    Create models

---
+++ lib/model/city.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_weather/model/weather.dart';
+
+class City {
+  final String title;
+  final int id;
+  final List<Weather> weathers;
+
+  City({
+    this.title,
+    this.id,
+    this.weathers,
+  });
+
+  Map<String, dynamic> toJson() => {
+        "title": title,
+        "woeid": id,
+        "weathers": weathers.map((e) => e.toJson()).toList(),
+      };
+
+  factory City.fromJson(Map<String, dynamic> map) {
+    final myWeathers = map['weathers'];
+    return City(
+      id: map['woeid'],
+      title: map['title'],
+      weathers: myWeathers != null
+          ? (myWeathers as List).map((e) => Weather.fromJson(e)).toList()
+          : null,
+    );
+  }
+
+  City fromWeathers(List<Weather> weathers) {
+    return City(
+      id: id,
+      title: title,
+      weathers: weathers,
+    );
+  }
+}
